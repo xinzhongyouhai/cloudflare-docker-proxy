8b5172299a1d93938ab1420c389bcdc8aeddf90b
sea in my heart <128223360+xinzhongyouhai@users.noreply.github.com>
2025-05-31 18:55:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.bitbot.top": "https://registry-1.docker.io",
+  "quay.bitbot.top": "https://quay.io",
+  "gcr.bitbot.top": "https://gcr.io",
+  "k8s-gcr.bitbot.top": "https://k8s.gcr.io",
+  "k8s.bitbot.top": "https://registry.k8s.io",
+  "ghcr.bitbot.top": "https://ghcr.io",
+  "cloudsmith.bitbot.top": "https://docker.cloudsmith.io",
+};
 
   // staging
   "docker-staging.lixd.xyz": dockerHub,
